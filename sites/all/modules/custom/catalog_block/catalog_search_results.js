cb50739d634f01726f4d181b6d90d75d3370b8c4
kevinreiss <kevin.reiss@gmail.com>
2012-08-16 00:23:09 -0400

    first pass at all search

--- sites/all/modules/custom/catalog_block/catalog_search_results.js
+++ sites/all/modules/custom/catalog_block/catalog_search_results.js
@@ -1,7 +1,20 @@
 (function ($) {
   $(document).ready(function() {
   	
-  	$('#catalog-search-results').replaceWith('<p>Crazy Time</p>');
-  	
+	//	$('#catalog-search-results').replaceWith('<p>Crazy Time</p>')	
+	var query = "cats";
+	console.log(query);
+        $.getJSON('/searchit/find/any/cats', function(data) {
+  		var items = [];
+
+  		$.each(data, function(key, val) {
+    			items.push('<li id="' + key + '">' + val + '</li>');
+  		});
+                console.log(items);
+  		$('<ul/>', {
+    			'class': 'my-new-list',
+    			html: items.join('')
+  		}).appendTo('#catalog-search-results');
+	});	
   });
 }(jQuery));
