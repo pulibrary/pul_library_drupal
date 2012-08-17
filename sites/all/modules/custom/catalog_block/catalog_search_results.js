27c87074385dcd76c3f1ee1d2a0724d92bc39184
kevinreiss <kevin.reiss@gmail.com>
2012-08-17 11:19:45 -0400

    working all search demo

--- sites/all/modules/custom/catalog_block/catalog_search_results.js
+++ sites/all/modules/custom/catalog_block/catalog_search_results.js
@@ -1,24 +1,28 @@
 (function ($) {
   $(document).ready(function() {
   	
-	//	$('#catalog-search-results').replaceWith('<p>Crazy Time</p>')	
-	var query = "firestone";
-	console.log(query);
-        $.getJSON('/searchit/find/any/'+query, function(data) {
-  		var items = [];
-		var more_link = data.more;
-		var num_results = data.number;
-		var records = data.records;
-		console.log(records);
-  		$.each(records, function(result) {
-			console.log(records[0]);
-    			items.push('<li><a href="' + result.url + '" target="_blank">' + result.title + '</a></li>');
-  		});
-                console.log(items);
-  		$('<ul/>', {
-    			'class': 'my-new-list',
-    			html: items.join('')
-  		}).appendTo('#catalog-search-results');
-	});	
+	//var query = "firestone";
+	var request = Drupal.settings.catalog_block.request;
+	//console.log(document.location.href);
+	var path = $(location).attr('pathname');
+	var query = path.substr(10);
+	if(query === "" || query == undefined) {
+		$('<div class="message">Please supply search terms</div>').appendTo('#catalog-search-results');
+	} else {
+        	$.getJSON('/searchit/find/any/'+query, function(data) {
+  			var items = [];
+			var more_link = data.more;
+			var num_results = data.number;
+			var records = data.records;
+  			$.each(records, function(index, result) {
+    				items.push('<li><a href="' + result['url'] + '" target="_blank">' + result['title'] + '</a></li>');
+			});
+  			$('<ul/>', {
+    				'class': 'catalog-search-results-list',
+    				html: items.join('')
+  			}).appendTo('#catalog-search-results');
+			$('<div class="more-results"><a href="'+more_link+'">More Books+ Results</a></div>"').appendTo('#catalog-search-results');
+		});
+	}	
   });
 }(jQuery));
