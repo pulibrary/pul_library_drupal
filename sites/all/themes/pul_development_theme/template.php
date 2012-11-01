76e74008c0b1bdb2ea45e4a1bb76cc5c9d452f3b
Lee Lilly <llilly@princeton.edu>
2012-11-01 18:07:24 -0400

    CSS Breadcrumbs & sidebars

--- sites/all/themes/pul_development_theme/template.php
+++ sites/all/themes/pul_development_theme/template.php
@@ -19,6 +19,19 @@ drupal_add_library('system', 'ui.accordion');
 drupal_add_library('system', 'ui.button');
 drupal_add_library('system', 'ui.progressbar');
 
+function pul_development_theme_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode($breadcrumb) . '</div>';
+    return $output;
+  }
+}
+
 
 // hacky way to rewrite menu html
 /*
