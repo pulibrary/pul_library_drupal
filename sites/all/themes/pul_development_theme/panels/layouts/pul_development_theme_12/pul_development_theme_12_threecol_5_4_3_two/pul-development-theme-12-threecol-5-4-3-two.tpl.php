d6fd39ba30b6859e79fc1d3dbaaad0413cc1c3dc
Kevin Reiss <kevin.reiss@gmail.com>
2012-12-06 12:55:57 -0500

    new 5-4-3 panel layout

---
+++ sites/all/themes/pul_development_theme/panels/layouts/pul_development_theme_12/pul_development_theme_12_threecol_5_4_3_two/pul-development-theme-12-threecol-5-4-3-two.tpl.php
@@ -0,0 +1,20 @@
+<div class="panel-display omega-grid omega-12-threecol-5-4-3" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-5">
+    <div class="inside"><?php print $content['left_top']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['middle_top']; ?></div>
+  </div>
+  <div class="panel-panel grid-3">
+    <div class="inside"><?php print $content['right_top']; ?></div>
+  </div>
+  <div class="panel-panel grid-5">
+    <div class="inside"><?php print $content['left_bottom']; ?></div>
+  </div>
+  <div class="panel-panel grid-4">
+    <div class="inside"><?php print $content['middle_bottom']; ?></div>
+  </div>
+  <div class="panel-panel grid-3">
+    <div class="inside"><?php print $content['right_bottom']; ?></div>
+  </div>
+</div>
