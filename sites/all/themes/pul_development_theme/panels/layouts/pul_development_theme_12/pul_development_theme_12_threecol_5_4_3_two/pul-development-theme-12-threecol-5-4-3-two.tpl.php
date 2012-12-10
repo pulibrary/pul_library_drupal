def56af9b97f56dcbf52e2ba05552075ad7350eb
Kevin Reiss <kevin.reiss@gmail.com>
2012-12-10 10:42:49 -0500

    5-4-3 grid updates

--- sites/all/themes/pul_development_theme/panels/layouts/pul_development_theme_12/pul_development_theme_12_threecol_5_4_3_two/pul-development-theme-12-threecol-5-4-3-two.tpl.php
+++ sites/all/themes/pul_development_theme/panels/layouts/pul_development_theme_12/pul_development_theme_12_threecol_5_4_3_two/pul-development-theme-12-threecol-5-4-3-two.tpl.php
@@ -1,20 +1,24 @@
-<div class="panel-display omega-grid omega-12-threecol-5-4-3" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel grid-5">
-    <div class="inside"><?php print $content['left_top']; ?></div>
-  </div>
-  <div class="panel-panel grid-4">
-    <div class="inside"><?php print $content['middle_top']; ?></div>
+<div class="panel-display omega-grid omega-12-twocol-9-3 omega-12-twocol-two-box" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-9">
+    <div class="panel-panel grid-5 alpha">
+    	<div class="inside"><?php print $content['left_top']; ?></div>
+    </div>
+    <div class="panel-panel grid-4">
+        <div class="inside"><?php print $content['middle_top']; ?></div>
+    </div>
+    <div class="panel-panel grid-5 alpha">
+        <div class="inside"><?php print $content['left_bottom']; ?></div>
+    </div>
+    <div class="panel-panel grid-4">
+    	<div class="inside"><?php print $content['middle_bottom']; ?></div>
+    </div>
+    <div class="panel-panel grid-9 alpha omega">
+        <div class="inside"><?php print $content['bottom']; ?></div>
+
+    </div>
   </div>
   <div class="panel-panel grid-3">
     <div class="inside"><?php print $content['right_top']; ?></div>
   </div>
-  <div class="panel-panel grid-5">
-    <div class="inside"><?php print $content['left_bottom']; ?></div>
-  </div>
-  <div class="panel-panel grid-4">
-    <div class="inside"><?php print $content['middle_bottom']; ?></div>
-  </div>
-  <div class="panel-panel grid-3">
-    <div class="inside"><?php print $content['right_bottom']; ?></div>
-  </div>
+  
 </div>
