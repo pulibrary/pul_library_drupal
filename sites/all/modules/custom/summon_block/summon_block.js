1ff4280543d0b13b675ef727160303b0e9510ee6
Kevin Reiss <kevin.reiss@gmail.com>
2012-12-18 11:12:53 -0500

    fix homepage validation errors

--- sites/all/modules/custom/summon_block/summon_block.js
+++ sites/all/modules/custom/summon_block/summon_block.js
@@ -8,8 +8,10 @@
     	//alert($(this).find("input:first").val());
     	console.log(Drupal.settings.catalog_block.catalog_search_hint);
     	if ($(this).find("input:first").val() == "" || $(this).find("input:first").val() == Drupal.settings.summon_block.summon_search_hint) {
-    		$("#content,#section-content").prepend("<div id='messages'><div class='messages error'><h2>Supply a Search Term</h2></div></div>");
-    		return false;
+		if($('#summon-error').length == 0) {
+		 	$("<div id='messages' class='grid-12'><div id='summon-error' class='messages error'><h2 class='element-invisible'>Error message</h2>Supply a Search Term</h2></div></div>").insertBefore('#region-content');
+    		}
+		return false;
     	}
     	
     	return true;
