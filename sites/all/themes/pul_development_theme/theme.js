62003dda48388c615ed80b06d20bc2ccff13bd21
antonioG4 <abarrera@princeton.edu>
2013-05-29 08:44:42 -0400

    added line to allow for new Libraries and Collections CT menu

--- sites/all/themes/pul_development_theme/theme.js
+++ sites/all/themes/pul_development_theme/theme.js
@@ -6,7 +6,9 @@
         
         $('.main-menu-item-title').replaceWith('<a href="/libraries">Libraries and Collections</a>');
         $('.view-display-id-block_1 .item-list:eq(1) ul').append("<li class='last leaf more'><a href='/collections' title='See All Collections'>More Collections</a></li>");
-	//FIXME hack should be repaced by a module that allows viewOnlineLink to be set a drupal conf variable
+        $('.view-libraries-and-collections-combined .item-list:eq(1) ul').append("<li class='last leaf more'><a href='/collections' title='See All Collections'>More Collections</a></li>");
+
+        //FIXME hack should be repaced by a module that allows viewOnlineLink to be set a drupal conf variable
 	$('.node-type-database .field-name-field-db-access-url a, .node-type-alternative-database-title .field-name-field-db-access-url a').each(function(index){
 		var viewOnlineLink = $(this).attr('href');
 		undecodeViewOnline = viewOnlineLink.replace( /\&amp%3B|&amp;/g, '&' );
