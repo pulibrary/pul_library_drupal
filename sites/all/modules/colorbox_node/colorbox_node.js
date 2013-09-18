ace8cf4bb41e1d656fa01c22034b73a6ee4dae84
Kevin Reiss <kevin.reiss@gmail.com>
2013-09-17 22:29:09 -0400

    colorbox_node 7.3.1 update

--- sites/all/modules/colorbox_node/colorbox_node.js
+++ sites/all/modules/colorbox_node/colorbox_node.js
@@ -1,86 +1,164 @@
-(function($) {
-	Drupal.behaviors.colorboxNode = {
-		// Lets find our class name and change our URL to
-		// our defined menu path to open in a colorbox modal.
-		attach : function(context, settings) {
-			$.urlParams = function (url) {
-		      var p = {},
-		          e,
-		          a = /\+/g,  // Regex for replacing addition symbol with a space
-		          r = /([^&=]+)=?([^&]*)/g,
-		          d = function (s) { return decodeURIComponent(s.replace(a, ' ')); },
-		          q = url.split('?');
-		      while (e = r.exec(q[1])) {
-		        e[1] = d(e[1]);
-		        e[2] = d(e[2]);
-		        switch (e[2].toLowerCase()) {
-		          case 'true':
-		          case 'yes':
-		            e[2] = true;
-		            break;
-		          case 'false':
-		          case 'no':
-		            e[2] = false;
-		            break;
-		        }
-		        if (e[1] == 'width') { e[1] = 'innerWidth'; }
-		        if (e[1] == 'height') { e[1] = 'innerHeight'; }
-		        p[e[1]] = e[2];
-		      }
-		      return p;
-		    };
-		
-		    $('.colorbox-node', context).once('init-colorbox-node-processed', function() {
-		    	var href = $(this).attr('data-href');
-		    	if(typeof href == 'undefined' || href == false) {
-		    		href = $(this).attr('href');
-		    	}
-				// Create an element so we can parse our a URL no matter if its internal or external.
-				var parse = document.createElement('a');
-				parse.href = href;
-				// Lets add our colorbox link after the base path if necessary.
-				var base_path = Drupal.settings.basePath;
-				var pathname = parse.pathname;
+(function ($) {
+    Drupal.behaviors.colorboxNode = {
+        // Lets find our class name and change our URL to
+        // our defined menu path to open in a colorbox modal.
+        attach: function (context, settings) {
+            $('.colorbox-node', context).once('init-colorbox-node-processed', function () {
+                $(this).colorboxNode({'launch': false});
+            });
 
-				// Lets check to see if the pathname has a forward slash.
-				// This problem happens in IE7/IE8
-				if(pathname.charAt(0) != '/') {
-					pathname = '/' + parse.pathname;
-				}
-				
-				if(base_path != '/') {
-					var link = pathname.replace(base_path, base_path + 'colorbox/') + parse.search;
-				} else {
-					var link = base_path + 'colorbox' + pathname + parse.search;
-				}
-				
-				// Bind Ajax behaviors to all items showing the class.
-			    var element_settings = {};
-				
-			    // This removes any loading/progress bar on the clicked link
-				// and displays the colorbox loading screen instead.
-				element_settings.progress = { 'type': 'none' };
-				$(this).click(function() {
-					var params = $.urlParams($(this).attr('href'));
-					params.html = '<div id="colorboxNodeLoading"></div>';
-			        $.colorbox($.extend({}, settings.colorbox, params));
-				});
-	
-			    // For anchor tags, these will go to the target of the anchor rather
-			    // than the usual location.
-			    if ($(this).attr('href')) {
-			    	element_settings.url = link;
-			    	element_settings.event = 'click';
-			    }
-			    var base = $(this).attr('id');
-			    Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
-			});			
-			
-			// When using contextual links and clicking from within the colorbox
-			// we need to close down colorbox when opening the built in overlay.
-			$('ul.contextual-links a', context).once('colorboxNodeContextual').click(function() {
-				$.colorbox.close();
-			});
-		}
-	};
+            // When using contextual links and clicking from within the colorbox
+            // we need to close down colorbox when opening the built in overlay.
+            $('ul.contextual-links a', context).once('colorboxNodeContextual').click(function () {
+                $.colorbox.close();
+            });
+        }
+    };
+
+    // Bind our colorbox node functionality to an anchor
+    $.fn.colorboxNode = function (options) {
+        var settings = {
+            'launch': true
+        };
+
+        $.extend(settings, options);
+
+        var href = $(this).attr('data-href');
+        if (typeof href == 'undefined' || href == false) {
+            href = $(this).attr('href');
+        }
+        // Create an element so we can parse our a URL no matter if its internal or external.
+        var parse = document.createElement('a');
+        parse.href = href;
+        // Lets add our colorbox link after the base path if necessary.
+        var base_path = Drupal.settings.basePath;
+        var pathname = parse.pathname;
+
+        // Lets check to see if the pathname has a forward slash.
+        // This problem happens in IE7/IE8
+        if (pathname.charAt(0) != '/') {
+            pathname = '/' + parse.pathname;
+        }
+
+        if (base_path != '/') {
+            var link = pathname.replace(base_path, base_path + 'colorbox/') + parse.search;
+        } else {
+            var link = base_path + 'colorbox' + pathname + parse.search;
+        }
+
+        // Bind Ajax behaviors to all items showing the class.
+        var element_settings = {};
+
+        // This removes any loading/progress bar on the clicked link
+        // and displays the colorbox loading screen instead.
+        element_settings.progress = { 'type': 'none' };
+        // For anchor tags, these will go to the target of the anchor rather
+        // than the usual location.
+        if (href) {
+            element_settings.url = link;
+            element_settings.event = 'click';
+        }
+
+        $(this).click(function () {
+            $this = $(this);
+
+            // Lets extract our width and height giving priority to the data attributes.
+            var innerWidth = $this.data('inner_width');
+            var innerHeight = $this.data('inner_height');
+            if (typeof innerWidth != 'undefined' && typeof innerHeight != 'undefined') {
+                var params = $.urlDataParams(innerWidth, innerHeight);
+            } else {
+                var params = $.urlParams(href);
+            }
+
+            params.html = '<div id="colorboxNodeLoading"></div>';
+            params.onComplete = function () {
+                $this.colorboxNodeGroup();
+            }
+
+            // Launch our colorbox with the provided settings
+            $.colorbox($.extend({}, Drupal.settings.colorbox, params));
+        });
+
+        // Log our click handler to our ajax object
+        var base = $(this).attr('id');
+        Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
+
+        // Default to auto click for manual call to this function.
+        if (settings.launch) {
+            Drupal.ajax[base].eventResponse(this, 'click');
+            $(this).click();
+        }
+    }
+
+    // Allow for grouping on links to showcase a gallery with left/right arrows.
+    // This function will find the next index of each link on the page by the rel
+    // and manually force a click on the link to call that AJAX and update the
+    // modal window.
+    $.fn.colorboxNodeGroup = function () {
+        // Lets do something wonky with galleries.
+        var rel = $(this).attr('rel');
+        if ($('a[rel="' + rel + '"]:not("#colorbox a[rel="' + rel + '"]")').length > 1) {
+            $related = $('a[rel="' + rel + '"]:not("#colorbox a[rel="' + rel + '"]")');
+            var idx = $related.index($(this));
+
+            // Show our gallery buttons
+            $('#cboxPrevious, #cboxNext').show();
+            $.colorbox.next = function () {
+                index = getIndex(1);
+                $related[index].click();
+
+            };
+            $.colorbox.prev = function () {
+                index = getIndex(-1);
+                $related[index].click();
+            };
+        }
+
+        function getIndex(increment) {
+            var max = $related.length;
+            var newIndex = (idx + increment) % max;
+            return (newIndex < 0) ? max + newIndex : newIndex;
+        }
+    }
+
+    // Utility function to parse out our width/height from our url params
+    $.urlParams = function (url) {
+        var p = {},
+            e,
+            a = /\+/g,  // Regex for replacing addition symbol with a space
+            r = /([^&=]+)=?([^&]*)/g,
+            d = function (s) {
+                return decodeURIComponent(s.replace(a, ' '));
+            },
+            q = url.split('?');
+        while (e = r.exec(q[1])) {
+            e[1] = d(e[1]);
+            e[2] = d(e[2]);
+            switch (e[2].toLowerCase()) {
+                case 'true':
+                case 'yes':
+                    e[2] = true;
+                    break;
+                case 'false':
+                case 'no':
+                    e[2] = false;
+                    break;
+            }
+            if (e[1] == 'width') {
+                e[1] = 'innerWidth';
+            }
+            if (e[1] == 'height') {
+                e[1] = 'innerHeight';
+            }
+            p[e[1]] = e[2];
+        }
+        return p;
+    };
+
+    // Utility function to return our data attributes for width/height
+    $.urlDataParams = function (innerWidth, innerHeight) {
+        return {'innerWidth':innerWidth,'innerHeight':innerHeight};
+    };
+
 })(jQuery);
