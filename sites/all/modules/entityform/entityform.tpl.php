5ee339a9670ea4104678a328ea7f84468cc8cabb
Kevin Reiss <kevin.reiss@gmail.com>
2013-11-21 18:49:28 -0500

    emtity form 1.3 update

--- sites/all/modules/entityform/entityform.tpl.php
+++ sites/all/modules/entityform/entityform.tpl.php
@@ -5,7 +5,7 @@
  * A basic template for entityform entities
  *
  * Available variables:
- * - $content: An array of comment items. Use render($content) to print them all, or
+ * - $content: An array of field items. Use render($content) to print them all, or
  *   print a subset such as render($content['field_example']). Use
  *   hide($content['field_example']) to temporarily suppress the printing of a
  *   given element.
