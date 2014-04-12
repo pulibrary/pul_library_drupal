9352a46122eb512f05e90f90b838320005bd328d
Kevin Reiss <kevin.reiss@gmail.com>
2014-04-12 08:02:36 -0400

    New 9-3 stacked layout

---
+++ sites/all/themes/pul_development_theme/panels/layouts/pul_development_theme_12/pul_development_theme_12_twocol_9_3_stacked/pul-development-theme-12-twocol-9-3-stacked.tpl.php
@@ -0,0 +1,14 @@
+<div class="panel-display omega-grid pul-development-theme-12-twocol-9-3-stacked" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['top']; ?></div>
+  </div>
+  <div class="panel-panel grid-9 alpha">
+    <div class="inside"><?php print $content['left']; ?></div>
+  </div>
+  <div class="panel-panel grid-3 omega">
+    <div class="inside"><?php print $content['right']; ?></div>
+  </div>
+  <div class="panel-panel grid-12">
+    <div class="inside"><?php print $content['bottom']; ?></div>
+  </div>
+</div>
