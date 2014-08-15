cf85d8ab61690a1af6eab3ce9c8af379b8f8632c
Axa Mei Liauw <aliauw@princeton.edu>
2014-08-15 15:19:21 -0400

    refactor landing page componenets to sass

--- sites/all/themes/pul_base/templates/system/page.tpl.php
+++ sites/all/themes/pul_base/templates/system/page.tpl.php
@@ -94,7 +94,10 @@
     </div>
     
     <?php print render($page['header']); ?>
-    <div class="wrapper wrapper--navigation"><?php print render($page['navigation']); ?></div>
+    <div class="wrapper wrapper--navigation">
+      <a href="" class="centered-navigation-menu-button">MENU</a>
+      <?php print render($page['navigation']); ?>
+    </div>
     <div class="wrapper wrapper--highlighted"><?php print render($page['highlighted']); ?></div>
   </header>
 
