7d4ecdf6f4984cc3dc58912cb80e9e1e78421e8f
Kevin Reiss <kevin.reiss@gmail.com>
2014-08-28 00:30:22 -0400

    panel creation

---
+++ sites/all/themes/pul_base/panels/layouts/nine_three_stacked/nine-three-stacked.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * Template for the Landing Page layout.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+<div<?php print $attributes ?>>
+  <div<?php print drupal_attributes($region_attributes_array['top'])?>>
+    <?php print $content['top']; ?>
+  </div>
+  <div<?php print drupal_attributes($region_attributes_array['first'])?>>
+    <?php print $content['first']; ?>
+  </div>
+  <div<?php print drupal_attributes($region_attributes_array['second'])?>>
+    <?php print $content['second']; ?>
+  </div>
+  <div<?php print drupal_attributes($region_attributes_array['bottom'])?>>
+    <?php print $content['bottom']; ?>
+  </div>
+</div>
