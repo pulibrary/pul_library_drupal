dabb4b5acd268e83e4d0adbdd46e0272828f2c2b
Kevin Reiss <kevin.reiss@gmail.com>
2014-09-13 13:30:39 -0400

    new calendar templates

--- sites/all/themes/pul_base/templates/calendar/calendar-month-col.tpl.php
+++ sites/all/themes/pul_base/templates/calendar/calendar-month-col.tpl.php
@@ -12,6 +12,6 @@ $headers = (isset($item['header_id'])) ? ' headers="'. $item['header_id'] .'" ' 
 ?>
 <td <?php print $id?>class="<?php print $item['class'] ?>" colspan="<?php print $item['colspan'] ?>" rowspan="<?php print $item['rowspan'] ?>"<?php print $date . $headers . $day; ?>>
   <div class="inner">
-    <?php print $item['entry'] ?>
+    <?php print $item['entry']; ?>
   </div>
 </td>
