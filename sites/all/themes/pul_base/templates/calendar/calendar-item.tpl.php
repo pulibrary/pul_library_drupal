e5e9d0fd282fd2e9af463de4086a2804b27b81e1
Axa Mei Liauw <aliauw@princeton.edu>
2014-11-12 11:31:47 -0500

    fix calendar header displays

--- sites/all/themes/pul_base/templates/calendar/calendar-item.tpl.php
+++ sites/all/themes/pul_base/templates/calendar/calendar-item.tpl.php
@@ -46,6 +46,7 @@ if (substr($item->date_id, -1)==0) {
 <div class="<?php print !empty($item->class) ? $item->class : 'item'; ?>">
   <div class="view-item view-item-<?php print $view->name ?>">
   <div class="calendar <?php print $item->granularity; ?>view">
+    <?php print $cur_week ?>
     <?php print theme('calendar_stripe_stripe', array('item' => $item)); ?>
     <div class="<?php print $item->date_id ?> contents">
       <?php foreach ($rendered_fields as $field): ?>
