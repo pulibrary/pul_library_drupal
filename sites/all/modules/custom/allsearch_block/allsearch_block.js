1479c83da9fb7bf283c6db4fcb07d6852e08d472
Axa Mei Liauw <aliauw@princeton.edu>
2015-06-01 13:11:37 -0400

    Relocate error block for all search

--- sites/all/modules/custom/allsearch_block/allsearch_block.js
+++ sites/all/modules/custom/allsearch_block/allsearch_block.js
@@ -9,7 +9,7 @@
      $('#allsearch-block-form').submit(function() {
         if ($(this).find("input:first").val() == "" || $(this).find("input:first").val() == Drupal.settings.allsearch_block.all_search_hint) {
                 if($('#allsearch-error').length == 0) {
-                  $("<div id='allsearch-error' class='alert--message'><h2 class='element-invisible'>Error message</h2><h3><i class='icon-alert'></i> "+Drupal.settings.allsearch_block.all_search_error+"</h3></div>").insertBefore('.l-content');
+                  $('.four_five_three_stacked-region--top').prepend("<div id='allsearch-error' class='alert--message'><h2 class='element-invisible'>Error message</h2><h3><i class='icon-alert'></i> "+Drupal.settings.allsearch_block.all_search_error+"</h3></div>");
             }
             
                 return false;
