355663ff64ec381b80690763211b9fbfbd3483a6
Kevin Reiss <kevin.reiss@gmail.com>
2015-07-06 10:13:55 -0400

    Updates for mollom, search_api, webform_valiation, redirect.

--- sites/all/modules/search_api_autocomplete/search_api_autocomplete.js
+++ sites/all/modules/search_api_autocomplete/search_api_autocomplete.js
@@ -9,32 +9,39 @@ if (typeof Drupal.jsAC != 'undefined') {
    * Extend from Drupal's autocomplete.js to automatically submit the form
    * when Enter is hit.
    */
-  Drupal.jsAC.defaultOnkeyup = Drupal.jsAC.prototype.onkeyup;
+  var default_onkeyup = Drupal.jsAC.prototype.onkeyup;
   Drupal.jsAC.prototype.onkeyup = function (input, e) {
     if (!e) {
       e = window.event;
     }
     // Fire standard function.
-    $.proxy(Drupal.jsAC.defaultOnkeyup, this)(input, e);
+    $.proxy(default_onkeyup, this)(input, e);
 
     if (13 == e.keyCode && $(input).hasClass('auto_submit')) {
-      $(':submit', input.form).trigger('click');
+      var selector;
+      if (typeof Drupal.settings.search_api_autocomplete.selector != 'undefined') {
+        selector = Drupal.settings.search_api_autocomplete.selector;
+      }
+      else {
+        selector = ':submit';
+      }
+      $(selector, input.form).trigger('click');
     }
   };
 
   /**
-   * Handler for the "keyup" event.
+   * Handler for the "keydown" event.
    *
    * Extend from Drupal's autocomplete.js to avoid ajax interfering with the
    * autocomplete.
    */
-  Drupal.jsAC.defaultOnkeydown = Drupal.jsAC.prototype.onkeydown;
+  var default_onkeydown = Drupal.jsAC.prototype.onkeydown;
   Drupal.jsAC.prototype.onkeydown = function (input, e) {
     if (!e) {
       e = window.event;
     }
     // Fire standard function.
-    $.proxy(Drupal.jsAC.defaultOnkeydown, this)(input, e);
+    $.proxy(default_onkeydown, this)(input, e);
 
     // Prevent that the ajax handling of views fires to early and thus
     // misses the form update.
@@ -47,27 +54,38 @@ if (typeof Drupal.jsAC != 'undefined') {
 
   Drupal.jsAC.prototype.select = function(node) {
     this.input.value = $(node).data('autocompleteValue');
+    $(this.input).trigger('autocompleteSelect', [node]);
     if ($(this.input).hasClass('auto_submit')) {
       if (typeof Drupal.search_api_ajax != 'undefined') {
         // Use Search API Ajax to submit
         Drupal.search_api_ajax.navigateQuery($(this.input).val());
-      } else {
-        $(':submit', this.input.form).trigger('click');
+      }
+      else {
+        var selector;
+        if (typeof Drupal.settings.search_api_autocomplete.selector != 'undefined') {
+          selector = Drupal.settings.search_api_autocomplete.selector;
+        }
+        else {
+          selector = ':submit';
+        }
+
+        $(selector, this.input.form).trigger('click');
       }
       return true;
     }
   };
 
   /**
-   * Overwrite default behaviour that would rely on synchronous jQuery animation
-   * in Drupal.jsAC.prototype.hidePopup() - which is simply impossible.
+   * Overwrite default behaviour.
    *
-   * Just don't return any boolean value.
+   * Just always return true to make it possible to submit even when there was
+   * an autocomplete suggestion list open.
    */
   Drupal.autocompleteSubmit = function () {
     $('#autocomplete').each(function () {
       this.owner.hidePopup();
     });
+    return true;
   };
 }
 
@@ -77,7 +95,7 @@ if (typeof Drupal.jsAC != 'undefined') {
 Drupal.ACDB.prototype.search = function (searchString) {
   this.searchString = searchString;
 
-  // Check allowed length of string for autocompete.
+  // Check allowed length of string for autocomplete.
   var data = $(this.owner.input).first().data('min-autocomplete-length');
   if (data && searchString.length < data) {
     return;
