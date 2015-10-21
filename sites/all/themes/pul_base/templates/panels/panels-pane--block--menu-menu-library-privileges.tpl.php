e3fde19266fac8979c51ab9dbb8029e2ab730386
Axa Mei Liauw <aliauw@princeton.edu>
2015-10-21 15:23:36 -0400

    Make section menus responsive. Closes #829 and closes #243

---
+++ sites/all/themes/pul_base/templates/panels/panels-pane--block--menu-menu-library-privileges.tpl.php
@@ -0,0 +1,12 @@
+<div class="menu--secondary">
+    <a href="#section-main" class="skip2main">Skip to Main Content</a>
+    <label class="show-section-menu" for="show-section-menu">Section Menu</label>
+    <input id="show-section-menu" type="checkbox">
+    <br>
+
+      <?php if ($admin_links): ?>
+        <?php print $admin_links; ?>
+      <?php endif; ?>
+
+      <?php print render($content); ?>
+</div>
