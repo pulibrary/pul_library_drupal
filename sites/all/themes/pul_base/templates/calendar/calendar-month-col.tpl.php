ca5624ea38f7b226a970c9db073ffe157bb6a6be
Axa Mei Liauw <aliauw@princeton.edu>
2016-04-22 17:05:12 -0400

    Make monthly calendar of library hours responsive. Closes #1112.

--- sites/all/themes/pul_base/templates/calendar/calendar-month-col.tpl.php
+++ sites/all/themes/pul_base/templates/calendar/calendar-month-col.tpl.php
@@ -2,14 +2,18 @@
 /**
  * @file
  * Template to display a column
- * 
+ *
  * - $item: The item to render within a td element.
  */
 $id = (isset($item['id'])) ? 'id="' . $item['id'] . '" ' : '';
 $date = (isset($item['date'])) ? ' data-date="' . $item['date'] . '" ' : '';
 $day = (isset($item['day_of_month'])) ? ' data-day-of-month="' . $item['day_of_month'] . '" ' : '';
 $headers = (isset($item['header_id'])) ? ' headers="'. $item['header_id'] .'" ' : '';
+$responsive_date = (isset($item['date'])) ?  date("l, F j, Y", strtotime($item['date'])) : '';
+$data_label = (isset($responsive_date)) ? ' data-label="' . $responsive_date . '" ' : '';
 ?>
-<td <?php print $id?>class="<?php print $item['class'] ?>" colspan="<?php print $item['colspan'] ?>" rowspan="<?php print $item['rowspan'] ?>"<?php print $date . $headers . $day; ?>>
-    <?php print $item['entry']; ?>
+<td <?php print $id?>class="<?php print $item['class'] ?>" colspan="<?php print $item['colspan'] ?>" rowspan="<?php print $item['rowspan'] ?>"<?php print $date . $headers . $day; ?> <?php print $data_label ?>>
+  <div class="inner">
+    <?php print $item['entry'] ?>
+  </div>
 </td>
