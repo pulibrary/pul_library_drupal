b77c2c21787c2a4470d814a925a69dfd5d82ea4d
Axa Mei Liauw <aliauw@princeton.edu>
2018-03-16 08:58:37 -0400

    Provide discernable text for logo

--- sites/all/themes/pul_base/templates/system/page.tpl.php
+++ sites/all/themes/pul_base/templates/system/page.tpl.php
@@ -80,7 +80,7 @@
     <div class="wrapper wrapper--branding">
       <section class="l-region l-region--branding">
         <?php if ($logo): ?>
-          <a href="<?php print $front_page; ?>" class="site-logo"><img src="/sites/all/themes/pul_base/assets/public/images/pul-logo-new.png" alt="<?php print t('Link to Princeton University Library home page'); ?>" /><span class="site-name"><?php print $site_name; ?></span></a>
+          <a href="<?php print $front_page; ?>" class="site-logo"><img src="/sites/all/themes/pul_base/assets/public/images/pul-logo-new.png" alt="<?php print t('Link to Princeton University Library home page'); ?>" /><span class="sr-only"><?php print $site_name; ?></span></a>
         <?php endif; ?>
         <?php print render($page['branding']); ?>
       </section>
