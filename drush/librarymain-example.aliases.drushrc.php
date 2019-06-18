1ee80d76951f1ba494ed773241f9264cbe6c02d4
Kevin Reiss <kevin.reiss@gmail.com>
2019-06-17 21:58:47 -0500

    First pass at lando

---
+++ drush/librarymain-example.aliases.drushrc.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Local alias
+ * Set the root and site_path values to point to your local site
+ */
+// $aliases['local'] = array(
+//   'root' => '', // Path to project on local machine
+//   'uri'  => 'http://rbscdrupal.lndo.site:8000',
+//   'path-aliases' => array(
+//     '%dump-dir' => '/tmp',
+//     '%files' => 'sites/default/files',
+//   ),
+// );
+
+/**
+ * Production alias
+ * Set each option to match your configuration
+ */
+// $aliases['prod'] = array (
+//   'uri' => 'https://library.princeton.edu',
+//   'root' => '', // Add root
+//   'remote-user' => '', // Add user
+//   'remote-host' => '', // Add host
+//   'ssh-options' => '', // Add ssh
+//   'path-aliases' => array(
+//     '%dump-dir' => '/tmp',
+//   ),
+//   'source-command-specific' => array (
+//     'sql-sync' => array (
+//       'no-cache' => TRUE,
+//       'structure-tables-key' => 'common',
+//     ),
+//   ),
+//   'command-specific' => array (
+//     'sql-sync' => array (
+//       'sanitize' => TRUE,
+//       'no-ordered-dump' => TRUE,
+//       'structure-tables' => array(
+//        // You can add more tables which contain data to be ignored by the database dump
+//         'common' => array('cache', 'cache_*', 'history', 'sessions', 'watchdog', 'cas_data_login', 'captcha_sessions'),
+//       ),
+//     ),
+//   ),
+// );
+?>
