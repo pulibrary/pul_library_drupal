76e3a37d441697b43e874bcd585a585bad9de6d5
Carolyn Cole <cac9@princeton.edu>
2021-12-10 11:23:33 -0500

    Add two new environments for rolling deploy to production

---
+++ config/deploy/production_rolla.rb
@@ -0,0 +1,11 @@
+set :files_dir, "production_files"
+
+server "library-prod1", user: fetch(:user), roles: %w{app drupal_primary}
+
+set :search_api_solr_host, 'library-solr-prod.princeton.edu'
+# TODO: This should be a production location
+set :search_api_solr_path, '/solr/library-prod'
+set :smtp_host, "lib-ponyexpr.princeton.edu"
+set :smtp_port, "25"
+set :redis_prefix, "library-prod"
+set :redis_host, "lib-redis.princeton.edu"
