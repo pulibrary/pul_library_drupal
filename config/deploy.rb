6d64a9effc081087614bb21e5808268c6cd483a5
Kevin Reiss <kevin.reiss@gmail.com>
2023-02-28 16:08:01 -0600

    Update cap db_dump task to also download file. (#1969)
    Use the db role for the dump.

--- config/deploy.rb
+++ config/deploy.rb
@@ -329,7 +329,11 @@ end
 
 desc "Database dump"
 task :database_dump do
-  on release_roles :db do
-    execute "sudo -u root -i mysqldump #{ fetch(:db_name) } | gzip > /tmp/dump.sql.gz"
-  end
+    date = Time.now.strftime("%Y-%m-%d")
+    file_name = "backup-#{date}-#{fetch(:stage)}"
+    on release_roles :db do
+      execute "sudo -u root -i mysqldump #{ fetch(:db_name) } > /tmp/#{file_name}.sql"
+      execute "gzip -f /tmp/#{file_name}.sql"
+      download! "/tmp/#{file_name}.sql.gz", "#{file_name}.sql.gz"
+    end
 end
