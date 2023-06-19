d0e7deaa2301aa8efcf9008aa94d07adeadfb5f2
Francis Kayiwa <kayiwa@pobox.com>
2023-06-19 09:16:22 -0400

    add new staging redis server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,13 +1,13 @@
-set :files_dir, "staging_files"
+set :files_dir, 'staging_files'
 
-server "library-staging1", user: fetch(:user), roles: %w{app drupal_primary}
-server "library-staging2", user: fetch(:user), roles: %w{app drupal_secondary}
+server 'library-staging1', user: fetch(:user), roles: %w[app drupal_primary]
+server 'library-staging2', user: fetch(:user), roles: %w[app drupal_secondary]
 
 set :search_api_solr_host, 'lib-solr8-staging.princeton.edu'
 set :search_api_solr_path, '/solr/library-staging'
-set :smtp_host, "localhost"
-set :smtp_port, "1025"
-set :redis_prefix, "library-staging"
-set :redis_host, "lib-redis.princeton.edu"
+set :smtp_host, 'localhost'
+set :smtp_port, '1025'
+set :redis_prefix, 'library-staging'
+set :redis_host, 'lib-redis-staging1.princeton.edu'
 
-set :db_name, "libwww_staging"
+set :db_name, 'libwww_staging'
