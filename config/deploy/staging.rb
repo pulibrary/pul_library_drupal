8a06b4fe17a25ebeaf58616362b8ecb188f85aeb
Francis Kayiwa <kayiwa@pobox.com>
2023-06-19 09:17:13 -0400

    add new staging server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,7 @@ set :files_dir, 'staging_files'
 
 server 'library-staging1', user: fetch(:user), roles: %w[app drupal_primary]
 server 'library-staging2', user: fetch(:user), roles: %w[app drupal_secondary]
+server 'library-staging3', user: fetch(:user), roles: %w[app drupal_secondary]
 
 set :search_api_solr_host, 'lib-solr8-staging.princeton.edu'
 set :search_api_solr_path, '/solr/library-staging'
