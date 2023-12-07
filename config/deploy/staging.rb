db860feb7d3251fe7fd9e5b07ae78f66b600f38f
Francis Kayiwa <kayiwa@pobox.com>
2023-12-07 09:35:24 -0500

    update library-staging1 to jammy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :files_dir, "staging_files"
 
-# server "library-staging1", user: fetch(:user), roles: %w{app drupal_primary}
+server "library-staging1", user: fetch(:user), roles: %w{app drupal_primary}
 server "library-staging2", user: fetch(:user), roles: %w{app drupal_secondary}
 
 set :search_api_solr_host, 'lib-solr8-staging.princeton.edu'
