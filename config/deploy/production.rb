e6e115c3294f3bb54df54e8c8a4278b2411f79f2
Kevin Reiss <kevin.reiss@gmail.com>
2023-12-19 15:24:49 -0600

    Move to new redis. (#2030)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,6 +10,6 @@ set :search_api_solr_path, '/solr/library-prod'
 set :smtp_host, "lib-ponyexpr.princeton.edu"
 set :smtp_port, "25"
 set :redis_prefix, "library-prod"
-set :redis_host, "lib-redis.princeton.edu"
+set :redis_host, "lib-redis-prod1.princeton.edu"
 
 set :db_name, "libwww_prod"
