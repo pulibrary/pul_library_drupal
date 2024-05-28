d672825502a6c7e5001c1311776a3a3989a122d0
Francis Kayiwa <kayiwa@pobox.com>
2024-05-28 13:30:38 -0400

    add new relay server

--- config/deploy/production_rollb.rb
+++ config/deploy/production_rollb.rb
@@ -6,7 +6,7 @@ server "library-prod4", user: fetch(:user), roles: %w{app drupal_secondary}
 set :search_api_solr_host, 'library-solr-prod.princeton.edu'
 # TODO: This should be a production location
 set :search_api_solr_path, '/solr/library-prod'
-set :smtp_host, "lib-ponyexpr.princeton.edu"
+set :smtp_host, "lib-ponyexpr-prod.princeton.edu"
 set :smtp_port, "25"
 set :redis_prefix, "library-prod"
 set :redis_host, "lib-redis.princeton.edu"
